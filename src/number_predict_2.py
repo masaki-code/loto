e46d3e90ec0334bc8468725aed4af9569c902212
masaki <masaki@masaki-blog.net>
2020-10-11 20:11:25 +0900

    リネーム

---
+++ src/number_predict_2.py
@@ -0,0 +1,110 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import RMSprop
+from keras.utils import np_utils
+
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# ファイル読み込み
+train_path = '../file/train_big.csv'
+test_path = '../file/test_big.csv'
+train = np.genfromtxt(train_path, delimiter=',', dtype='int')
+test = np.genfromtxt(test_path, delimiter=',', dtype='int')
+
+# ---------------------
+# 関数
+# ---------------------
+input_num = 1
+output_num = 43
+
+
+def vector_y(_x, num=output_num):
+    _list = np.empty((0, num), int)
+
+    for __x in _x:
+        n = np.add.reduce(np_utils.to_categorical(np.array(__x) - 1, num_classes=num))
+        n = np.array([n])
+        _list = np.append(_list, n, axis=0)
+
+    return _list
+
+
+# ---------------------
+# 訓練用データ
+# ---------------------
+x_train = train[:, 0]
+y_train = train[:, 2:]
+y_train = vector_y(y_train)
+
+# ---------------------
+# テスト（検証）用データ
+# ---------------------
+x_test = test[:, 0]
+y_test = test[:, 2:]
+y_test = vector_y(y_test)
+
+# ---------------------
+# モデル
+# ---------------------
+# 総データ数：約1,500
+# 訓練データ：1,000、テストデータ：500、くらいに分けてみる
+# バーニーおじさんのルールにより、パラメータ数は100くらいが理想？
+
+# 結果の次元が43あるのでパラメータ数を適切にするには難しそう。モデルを見直した方が良い？
+model = Sequential()
+model.add(Dense(6, activation='tanh', input_shape=(input_num,)))
+model.add(Dense(output_num, activation='softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+# ---------------------
+# 訓練
+# ---------------------
+batch_size = 128
+epochs = 20
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# ---------------------
+# 保存
+# ---------------------
+model.save('loto_model_big.h5')
+
+# ---------------------
+# 予測
+# ---------------------
+x = [251]
+result = model.predict(x, batch_size=None, verbose=0, steps=None)
+result = result[0]
+print('==================')
+print('result', result)
+print('==================')
+
+
+def predict_nums(_x):
+    k = 0
+    _disc = {}
+    for v in _x:
+        k += 1
+        _disc[k] = v
+
+    _sorted = sorted(_disc.items(), key=lambda __x: __x[1], reverse=True)
+    return sorted(list(map(lambda _x: _x[0], _sorted[::6])))
+
+
+print('==================')
+print(predict_nums(result))
+print('==================')
